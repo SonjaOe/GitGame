647932ec2945a883c4e0dab140a6dc72f8719107
Sonja <sonja.oehlandt@brightstraining.com>
2022-10-24 16:46:19 +0300

    commit sonja revised

--- GitGame.py
+++ GitGame.py
@@ -3,10 +3,10 @@ while True:
     letsplay = input(print("Do you want to play a game? (yes/no)"))
     if letsplay != yes:
         break
-    question1 = input(print("Montako varvasta kissalla on? a) 20 b) 18 c)16"))
+    question1 = input(print("Montako varvasta kissalla on? a) 20 b) 18 c) 16"))
     if question1 = "b" or "18":
         count += 1
-    question2 = input(print("Montako virallista koirarotua on olemassa? a) noin 500 b) noin 400 c) noin 300"))
-    if question2 = "c" or "300":
+    question2 = input(print("Montako virallista koirarotua on olemassa? a) noin 550 b) noin 450 c) noin 350"))
+    if question2 = "c" or "350":
         count += 1
 
