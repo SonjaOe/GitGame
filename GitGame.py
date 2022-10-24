087c2189b2eb7d8d4cf6b723357384e00ede2972
Katri <katri.palmroth@brightstraining.com>
2022-10-24 17:02:21 +0300

    lisätty kysymykset 5 ja 6

---
+++ GitGame.py
@@ -0,0 +1,35 @@
+while True:
+
+    count = 0
+
+    letsplay = input(print("Do you want to play a game? (yes/no)"))
+
+    if letsplay != yes:
+
+        break
+
+    question1 = input(print("Montako varvasta kissalla on? a) 20 b) 18 c) 16"))
+
+    if question1 = "b" or "18":
+
+        count += 1
+
+    question2 = input(print("Montako virallista koirarotua on olemassa? a) noin 550 b) noin 450 c) noin 350"))
+
+    if question2 = "c" or "350":
+
+        count += 1
+
+    
+    question5 = input(print("Miten vanha vanhimmaksi elänyt kissa oli kuollessaan? a) 39 b) 35 c) 32"))
+
+    if question1 = "a" or "39":
+
+        count += 1
+
+    question6 = input(print("Montako virallista kissarotua on olemassa Suomen kissaliiton mukaan? a) noin 60 b) noin 50 c) noin 40"))
+
+    if question2 = "b" or "50":
+
+        count += 1
+
