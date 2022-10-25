f2c382112a803458beea9d3e79de30008a334e01
Anssi Y-L <anssi.ylileppala@brightstraining.com>
2022-10-25 08:44:53 +0300

    Updated questions 3 and 4 and fixed question 1 and 2

--- GitGame.py
+++ GitGame.py
@@ -1,12 +1,34 @@
-while True:
-    count = 0
-    letsplay = input(print("Do you want to play a game? (yes/no)"))
-    if letsplay != yes:
-        break
-    question1 = input(print("Montako varvasta kissalla on? a) 20 b) 18 c)16"))
-    if question1 = "b" or "18":
-        count += 1
-    question2 = input(print("Montako virallista koirarotua on olemassa? a) noin 500 b) noin 400 c) noin 300"))
-    if question2 = "c" or "300":
-        count += 1
+letsplay = input("Haluatko pelata peliä? (kyllä/ei)\n")
 
+while True:
+    count = 0    
+    if letsplay != "kyllä":
+        print("Ei sitten! :(")
+        break
+    question1 = str(input("Montako varvasta kissalla on? a) 20 b) 18 c) 16\n"))
+    if question1 == "b" or question1 == "18":
+        count += 1
+        print("Oikein!")
+    else:
+        print("Väärin...")
+    question2 = str(input("Montako virallista koirarotua on olemassa? a) noin 500 b) noin 400 c) noin 300\n"))
+    if question2 == "c" or question2 == "300":
+        count += 1
+        print("Oikein!")
+    else:
+        print("Väärin...")        
+    question3 = str(input("Kuinka monta nenää etanalla on? a) 0 b) 1 c) 4\n"))
+    if question3 == "c" or question3 == "4":
+        count += 1
+        print("Oikein!")
+    else:
+        print("Väärin...")
+    question4 = str(input("Kuinka monet aivot meritähdellä on? a) 0 b) 1 c) 2\n"))
+    if question4 == "a" or question4 == "0":
+        count += 1
+        print("Oikein!")
+    else:
+        print("Väärin...")
+
+    print(f"Sait oikein {0}/6!")
+    break
